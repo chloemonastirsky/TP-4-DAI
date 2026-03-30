00d4b222b470baab046572391b20248b4f2547f1
CATALINA COHEN <49092736@ort.edu.ar>
2026-03-30 08:58:38 -0300

    corregir

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ const app = express()
 const port = 3000
 
 
-function GetHora(){
-    app.get('/', (req, res) => {
-        const time = new Time();
-        res.send(`La hora actual es: ${time.getTime()}`);
-    })
-}
+//  function GetHora(){
+//      app.get('/', (req, res) => {
+//          const time = new Time();
+//          res.send(`La hora actual es: ${time.getTime()}`);
+//      })
+//  }
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
 
+app.get('/hora', (req, res) => {
+    const time = new Time();
+    res.send(`La hora actual es: ${time.getTime()}`);
+})
+
+app.get('/fecha-completa', (req, res) => {
+    const time = new Time();
+    res.send(`La fecha y hora actual es: ${time.getTime()}`);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-    GetHora();
 }   )
